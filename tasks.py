e44f17ab597a0b86c117dd866b41aaa115af08ff
Feihong Hsu <feihong.hsu@gmail.com>
2016-11-07 12:48:50 -0600

    Start up webpack watch when the server starts
    And stop it when the server is stopped

--- tasks.py
+++ tasks.py
@@ -1,5 +1,10 @@
 import os
+import subprocess
+import signal
 from invoke import task
+
+
+WEBPACK_WATCH_COMMAND = 'webpack --progress --colors --debug --watch'
 
 
 @task
@@ -7,11 +12,16 @@ def serve(ctx):
     import http.server
     import socketserver
 
+    webpack_proc = subprocess.Popen(WEBPACK_WATCH_COMMAND, shell=True)
+
     Handler = http.server.SimpleHTTPRequestHandler
     httpd = socketserver.TCPServer(('localhost', 8000), Handler)
     print('Serving on localhost:8000')
     os.chdir('static')
     httpd.serve_forever()
+    # Server is stopped, so stop webpack as well.
+    webpack_proc.send_signal(signal.SIGINT)
+    webprack_proc.wait()
 
 
 @task
@@ -21,4 +31,4 @@ def build(ctx):
 
 @task
 def watch(ctx):
-    ctx.run('webpack --progress --colors --debug --watch')
+    ctx.run(WEBPACK_WATCH_COMMAND)
